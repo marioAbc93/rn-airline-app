75887fccce108c2ac65073fd1d9265f36d6250c5
marioAbc93 <mariobarros_93@hotmail.com>
2024-10-31 14:07:20 -0500

    fix: implement status bar in home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, View, Text, StyleSheet, Button } from "react-native";
+import { Image, View, Text, StyleSheet, Button, StatusBar } from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { BlurView } from "expo-blur";
 import { LinearGradient } from "expo-linear-gradient";
@@ -8,37 +8,40 @@ import ContactUsCta from "@/components/ContactUsCta";
 import RewardsRegisterCta from "@/components/RewardsRegisterCta";
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerImage={
-        <View className="overflow-hidden w-full h-[280px] relative">
-          <Image
-            source={require("@/assets/images/home-img.webp")}
-            className="w-full h-full"
-            resizeMode="cover"
-          />
+    <>
+      <StatusBar barStyle="light-content" />
+      <ParallaxScrollView
+        headerImage={
+          <View className="overflow-hidden w-full h-[280px] relative">
+            <Image
+              source={require("@/assets/images/home-img.webp")}
+              className="w-full h-full"
+              resizeMode="cover"
+            />
 
-          <LinearGradient
-            colors={["transparent", "#020C41"]}
-            style={StyleSheet.absoluteFillObject}
-            start={[0, 0]}
-            end={[0, 1]}
-          />
+            <LinearGradient
+              colors={["transparent", "#020C41"]}
+              style={StyleSheet.absoluteFillObject}
+              start={[0, 0]}
+              end={[0, 1]}
+            />
 
-          <BlurView
-            intensity={0}
-            className="absolute bottom-0 w-full pt-4 h-[100px] justify-center items-center"
-          >
-            <Text className="text-white text-3xl text-center font-extralight">
-              ¡Bienvenido a AeroTest!
-            </Text>
-          </BlurView>
-        </View>
-      }
-    >
-      <ReservationCta />
-      <BenefitsList />
-      <RewardsRegisterCta />
-      <ContactUsCta />
-    </ParallaxScrollView>
+            <BlurView
+              intensity={0}
+              className="absolute bottom-0 w-full pt-4 h-[100px] justify-center items-center"
+            >
+              <Text className="text-white text-3xl text-center font-extralight">
+                ¡Bienvenido a AeroTest!
+              </Text>
+            </BlurView>
+          </View>
+        }
+      >
+        <ReservationCta />
+        <BenefitsList />
+        <RewardsRegisterCta />
+        <ContactUsCta />
+      </ParallaxScrollView>
+    </>
   );
 }
